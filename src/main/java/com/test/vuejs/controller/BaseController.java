e781c09c2c7d456baadb05f585ce917b7e896376
이성민 <leesm7410@gmail.com>
2019-02-15 00:00:02 +0900

    test Setting

--- src/main/java/com/test/vuejs/controller/BaseController.java
+++ src/main/java/com/test/vuejs/controller/BaseController.java
@@ -2,12 +2,16 @@ package com.test.vuejs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BaseController {
 
     @RequestMapping("test")
-    public void test(){
+    public ModelAndView test() {
         System.out.print("hello test");
+        ModelAndView model = new ModelAndView();
+        model.setViewName("test");
+        return model;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,3 @@
+<jsp:include page="../views/template/header.jsp" flush="true"/>
+this is test pages
+<jsp:include page="../views/template/footer.jsp" flush="true"/>
