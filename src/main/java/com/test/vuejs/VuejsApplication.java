baac77ed7feec09f85dd7e6e00facd90ec3e9a26
root <leesm7410@gmail.com>
2019-11-07 10:16:22 +0900

    fix springBootinitializer

--- src/main/java/com/test/vuejs/VuejsApplication.java
+++ src/main/java/com/test/vuejs/VuejsApplication.java
@@ -2,9 +2,10 @@ package com.test.vuejs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VuejsApplication {
+public class VuejsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VuejsApplication.class, args);
